85499d18bb2ac0a27f3773bd8de70e7144301357
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-04-11 15:18:56 +0300

    Работа выполнена с использованием @ConfigurationProperties и @ConfigurationPropertiesScan

--- src/main/java/ru/gb/homework20240402/HomeworkApplication.java
+++ src/main/java/ru/gb/homework20240402/HomeworkApplication.java
@@ -2,8 +2,10 @@ package ru.gb.homework20240402;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("ru.gb.homework20240402.configuration")
 public class HomeworkApplication {
 
 	public static void main(String[] args) {
--- src/main/java/ru/gb/homework20240402/configuration/SqlStatementConfiguration.java
+++ src/main/java/ru/gb/homework20240402/configuration/SqlStatementConfiguration.java
@@ -3,9 +3,7 @@ package ru.gb.homework20240402.configuration;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @ConfigurationProperties(prefix = "sql.statements")
 @Getter
 @Setter
